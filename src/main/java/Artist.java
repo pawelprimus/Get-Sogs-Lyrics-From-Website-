b84dcc89a0e587969594a2fdf4312da96e8cd82f
Pawel <pawelprimus@gmail.com>
2020-04-26 16:37:56 +0200

    Main methods -> Artist Class

--- src/main/java/Artist.java
+++ src/main/java/Artist.java
@@ -1,5 +1,109 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Artist {
 
+    private static String name;
+  //  private static Map<String, Integer> wordOccurancy = new HashMap<>();
+    ArrayList<String> wordOccurancy = new ArrayList<String>();
 
+
+
+    public Artist(String name) {
+        this.name = name.replace(" ", "_");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<String> getWordOccurancy() {
+        return wordOccurancy;
+    }
+
+    public Map<String, Integer> getWordsAscending() {
+
+        Map<String, Integer> words = getWordsOccurances(getWordOccurancy());
+
+        Map<String, Integer> sortedMap = words.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
+        return sortedMap;
+    }
+
+
+    public ArrayList<String> getSongText() {
+        //List<String> list = Arrays.asList();
+        ArrayList<String> list = new ArrayList<String>();
+
+        List<String> URLlist = getSongLinks();
+
+        for (String url : URLlist) {
+            try {
+                Document document = Jsoup.connect(url).get();
+                Elements elements = document.getElementsByClass("song-text");
+                //String[] textArray = elements.text().toLowerCase().replaceAll("\\p{Punct}", "").split(" ");
+                String[] textArray = elements.text().toLowerCase().replaceAll("\\p{Punct}", "").split(" ");
+                list.addAll(Arrays.asList(textArray));
+
+               //System.out.println(list.toString());
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        wordOccurancy = list;
+
+        //System.out.println(list.toString());
+        return list;
+    }
+
+    public List<String> getSongLinks() {
+        String url = "https://www.tekstowo.pl/piosenki_artysty," + name.replace(" ", "_") + ",popularne,malejaco,strona,1.html";
+
+        List urlList = new ArrayList();
+
+        try {
+            Document doc = Jsoup.connect(url).ignoreHttpErrors(true).get();
+            Elements links = doc.getElementsByClass("ranking-lista").select("a[href]");
+            for (Element link : links) {
+                if (!link.attr("abs:href").isEmpty()) {
+                    urlList.add(link.attr("abs:href"));
+                } else {
+
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return urlList;
+    }
+
+    public Map<String, Integer> getWordsOccurances(ArrayList<String> words) {
+
+        ArrayList<String> list = words;
+
+
+
+        Set<String> uniqueWords = new HashSet<String>(list);
+        Map<String, Integer> frequencyWords = new HashMap<>();
+
+        for (String word : uniqueWords) {
+            //System.out.println(word + ": " + Collections.frequency(list, word));
+            frequencyWords.put(word,Collections.frequency(list, word));
+        }
+        return frequencyWords;
+
+    }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,103 +5,35 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // getSongText("Taco hemingway", "nastepna stacja");
 
-        //getWordsOccurances(getSongText("Taco hemingway", "nastepna stacja"));
-        //getSongsList("Taco Hemingway");
+        Artist artist = new Artist("Taco Hemingway");
 
-        // List<String> list = new ArrayList();
+        ArrayList<String> text = artist.getSongText();
 
-        // list = getSongLinks("Taco Hemingway");
+        Map<String, Integer> words = artist.getWordsOccurances(text);
+        System.out.println("Before sort" +artist.getWordOccurancy());
 
-        getSongLinks("Taco Hemingway");
-
-        getSongText("Taco Hemingway", getSongLinks("Taco Hemingway"));
-
+        System.out.println("After sort" + artist.getWordsAscending());
 
     }
 
 
-    public static String[] getSongText(String artist, List<String> URLlist) {
-
-
-        artist = artist.replace(" ", "_");
-        //title = title.replace(" ", "_");
-        //String url = "https://www.tekstowo.pl/piosenka," + artist + "," + title + ".html";
-        //  String url;
-
-
-        List<String> list = Arrays.asList();
-
-        Set<String> uniqueWords = new HashSet<String>(list);
-
-        for (String url : URLlist) {
-            try {
-                Document document = Jsoup.connect(url).get();
-                Elements elements = document.getElementsByClass("song-text");
-                String[] textArray = elements.text().toLowerCase().replaceAll("\\p{Punct}", "").split(" ");
-                //return textArray;
-
-
-                getWordsOccurances(textArray);
-
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        }
-        return null;
-    }
-
-    static void getWordsOccurances(String[] words) {
-
-        List<String> list = Arrays.asList(words);
-
-        Set<String> uniqueWords = new HashSet<String>(list);
-        Map<String, Integer> freqWords = new HashMap<>();
-
-
-
-        for (String word : uniqueWords) {
-            System.out.println(word + ": " + Collections.frequency(list, word));
-            freqWords.put(word,Collections.frequency(list, word));
-        }
 
 
 
 
-    }
 
 
-    static List<String> getSongLinks(String artist) {
 
 
-        String artistDash = artist.replace(" ", "_");
-        String url = "https://www.tekstowo.pl/piosenki_artysty," + artistDash + ",popularne,malejaco,strona,1.html";
 
-        List urlList = new ArrayList();
 
-        try {
-            Document doc = Jsoup.connect(url).ignoreHttpErrors(true).get();
-            Elements links = doc.getElementsByClass("ranking-lista").select("a[href]");
-            for (Element link : links) {
-                if (!link.attr("abs:href").isEmpty()) {
-                    urlList.add(link.attr("abs:href"));
-                } else {
 
-                }
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return urlList;
-    }
 }
