c2e58d66182bb1d81f9b90261efe5132bd4ad6e1
Pawel <pawelprimus@gmail.com>
2020-04-28 00:04:38 +0200

    Import to excel

--- src/main/java/Artist.java
+++ src/main/java/Artist.java
@@ -1,8 +1,17 @@
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -27,6 +36,8 @@ public class Artist {
         return allWords;
     }
 
+
+
     public Map<String, Integer> getWordsAscending() {
 
         Map<String, Integer> words = getMapWordsOccurances();
@@ -40,7 +51,6 @@ public class Artist {
 
 
     public void makeWordsList() {
-        //List<String> list = Arrays.asList();
         ArrayList<String> list = new ArrayList<String>();
 
         List<String> URLlist = getSongLinks();
@@ -88,10 +98,7 @@ public class Artist {
 
     public Map<String, Integer> getMapWordsOccurances() {
 
-
         ArrayList<String> allWordsList = getAllWords();
-
-
 
         Set<String> uniqueWords = new HashSet<String>(allWordsList);
         Map<String, Integer> frequencyWordsMap = new HashMap<>();
@@ -103,5 +110,71 @@ public class Artist {
 
         return frequencyWordsMap;
     }
+
+    public int getSumOfAllWords(){
+        return  getMapWordsOccurances().values().stream().mapToInt(i->i).sum();
+    }
+
+
+    public void importToExcel() throws IOException {
+        //create blank workbook
+        XSSFWorkbook workbook = new XSSFWorkbook();
+
+        //Create a blank sheet
+        XSSFSheet sheet = workbook.createSheet("AllWords");
+
+
+        int rownum = 0;
+        int cellnum = 0;
+
+        Row row = sheet.createRow(rownum++);
+
+        Cell cell = row.createCell(cellnum++);
+        cell.setCellValue("Words");
+        cell = row.createCell(cellnum++);
+        cell.setCellValue("Occurances");
+        cell = row.createCell(cellnum++);
+        cell.setCellValue("%");
+        cell = row.createCell(cellnum++);
+        cell.setCellValue("All words =");
+        cell = row.createCell(cellnum++);
+        cell.setCellValue(getSumOfAllWords());
+
+        Double allWords = Double.valueOf(getSumOfAllWords());
+
+        for(Map.Entry<String, Integer> entry : getWordsAscending().entrySet()){
+
+            cellnum=0;
+            row = sheet.createRow(rownum++);
+            cell = row.createCell(cellnum++);
+            cell.setCellValue(entry.getKey());
+            cell = row.createCell(cellnum++);
+            cell.setCellValue(entry.getValue());
+            cell = row.createCell(cellnum++);
+
+            Double thatWord = Double.valueOf(entry.getValue());
+
+            cell.setCellValue(thatWord/allWords);
+        }
+
+
+
+        try
+        {
+            //Write the workbook in file system
+            FileOutputStream out = new FileOutputStream(new File(getName()+"Words.xlsx"));
+            workbook.write(out);
+            out.close();
+            System.out.println(getName() + ".xlsx has been created successfully");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            workbook.close();
+        }
+    }
+
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,52 @@
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+import java.io.*;
 import java.util.*;
+
+
+
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
 
         Artist artist = new Artist("Taco Hemingway");
 
 
-        System.out.println("Before sort" +artist.getAllWords());
+       /* System.out.println("Before sort" +artist.getAllWords());
         System.out.println("Before sort with occurances" + artist.getMapWordsOccurances());
         System.out.println("After sort" + artist.getWordsAscending());
+
+        System.out.println("Sum of all words: " + artist.getSumOfAllWords());
+*/
+
+        artist.importToExcel();
+
 
 
 
     }
+
+
 
 
 
