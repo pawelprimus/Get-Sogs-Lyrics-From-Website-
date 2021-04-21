e1b0b2f42bf6739f831b9c31f23dde9913a042f4
Pawel P <pawelprimus@gmail.com>
2021-04-22 01:00:35 +0200

    Moved export excel from Artist to ExcelService

--- src/main/java/Artist.java
+++ src/main/java/Artist.java
@@ -129,60 +129,7 @@ public class Artist {
         String allSongs= getElementsFromUrlClass(url,"belka short").text().replaceAll("\\D+", "");
         return Integer.valueOf(allSongs);
     }
-
-    public void exportToExcel() throws IOException {
-        //create blank workbook
-        XSSFWorkbook workbook = new XSSFWorkbook();
-
-        //Create a blank sheet
-        XSSFSheet sheet = workbook.createSheet("AllWords");
-
-        int rownum = 0;
-        int cellnum = 0;
-
-        Row row = sheet.createRow(rownum++);
-
-        Cell cell = row.createCell(cellnum++);
-        cell.setCellValue("Words");
-        cell = row.createCell(cellnum++);
-        cell.setCellValue("Occurances");
-        cell = row.createCell(cellnum++);
-        cell.setCellValue("%");
-        cell = row.createCell(cellnum++);
-        cell.setCellValue("All words =" + getSumOfAllWords());
-        cell = row.createCell(cellnum++);
-        cell.setCellValue("Number of Songs =" + getNumberOfSongs());
-
-        Double allWords = Double.valueOf(getSumOfAllWords());
-
-        for (Map.Entry<String, Integer> entry : getWordsAscending().entrySet()) {
-
-            cellnum = 0;
-            row = sheet.createRow(rownum++);
-            cell = row.createCell(cellnum++);
-            cell.setCellValue(entry.getKey());
-            cell = row.createCell(cellnum++);
-            cell.setCellValue(entry.getValue());
-            cell = row.createCell(cellnum++);
-
-            Double thatWord = Double.valueOf(entry.getValue());
-
-            cell.setCellValue(thatWord / allWords);
-        }
-
-        try {
-            //Write the workbook in file system
-            FileOutputStream out = new FileOutputStream(new File(getName() + "Words.xlsx"));
-            workbook.write(out);
-            out.close();
-            System.out.println(getName() + ".xlsx has been created successfully");
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            workbook.close();
-        }
-    }
-
+    
 
     public List<String> getAllSongLinks() {  //GET ALL OF exists songs
 
